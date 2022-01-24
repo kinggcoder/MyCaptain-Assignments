8334c103dce1b67386d27092cf8b17fa748a307d
ALLEN ANTO JOSEPH <97299542+kinggcoder@users.noreply.github.com>
2022-01-24 15:03:57 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,3 @@
+a=int(input('Enter radious of the circle'))
+b=(22/7)*(a**2)
+print('Area of the circle with radius',a,'is',b)
---
+++ extention.py
@@ -0,0 +1,3 @@
+a=input('Enter filename:')
+f=a.split('.')
+print('Extention of file is',f[-1])
